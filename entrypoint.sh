16e6b3a3ecca1c293351154bd51bcc46303130f5
eltonplima <eltonplima@gmail.com>
2018-04-05 17:48:05 -0300

    Set supervisord config file param.

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ then
         echo "The redis master address is: " ${REDIS_HOST}
         echo "Starting services..."
         sed -i "s@REDIS_HOST=\"localhost\"@REDIS_HOST=\"$REDIS_HOST\"@g" $HOME/supervisord.conf
-        /usr/local/bin/supervisord&
+        /usr/local/bin/supervisord -c /usr/local/etc/supervisord.conf&
     else
         echo "Redis master not found!"
         exit 1
@@ -35,5 +35,6 @@ then
         sleep ${SENTINEL_CHECK_DELAY}
     done
 else
-    /usr/local/bin/supervisord
+    echo "SENTINEL_HOST is not defined."
+    exit 1
 fi
